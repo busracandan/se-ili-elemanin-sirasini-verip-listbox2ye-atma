88f3af71041f7e29d7a6f4557c5c9d34e2bba1bd
busracandan <busra58cndn@hotmail.com>
2018-02-24 15:55:43 +0200

    besinci projem

---
+++ çalışma sorusu 4/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace çalışma_sorusu_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem == null)
+            {
+                MessageBox.Show("eleman seçın");
+            }
+            else
+            MessageBox.Show(listBox1.SelectedIndex.ToString());
+           
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = listBox1.SelectedIndices.Count - 1; i >= 0; i--)
+            {
+                listBox2.Items.Add(listBox1.SelectedItems[i]);
+            }
+        }
+    }
+}
